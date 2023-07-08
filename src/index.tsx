3634a86ad795c7579db45bdfa1615837887e36bd
GeraN7 <gera3050@yandex.ru>
2023-07-08 16:58:36 +0300

    Описал компоненты страниц  и карточки предложения.

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './components/app/app';
+import { AppData } from './const';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offersCount={AppData.offersCount} />
   </React.StrictMode>
 );
