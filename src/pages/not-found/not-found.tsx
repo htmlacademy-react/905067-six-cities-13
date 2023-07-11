e30c5fad3ad79390c6881890da4ec34124c9217b
GeraN7 <gera3050@yandex.ru>
2023-07-11 21:37:31 +0300

    Подключил стили для страницы not-found

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import './not-found.css'
 function NotFound(): JSX.Element {
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div className='not-found__container'>
       <Helmet>
         <title>6 cities. 404</title>
       </Helmet>
       <h1>404 Not Found</h1>
       <p>К сожалению такой страницы не существует.</p>
-      <Link to="/" style={{ textDecoration: 'underline' }}>
+      <Link className='not-found__link' to="/">
         Вернуться на главную страницу
       </Link>
     </div>
